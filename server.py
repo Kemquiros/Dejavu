22d0929f26ea567582ce78a94a11ad2f2408f2cd
kemquiros <absurdoeco@gmail.com>
2018-01-15 22:53:55 -0500

    Added 404 page

--- server.py
+++ server.py
@@ -172,7 +172,9 @@ def get_board():
                          turno=partida.turnoActual,
                          tiempo=partida.getTime()
                         )
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
 	#app.debug = True #Uncomment to enable debugging
